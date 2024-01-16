a4ec9877248b3486752d7903245b76edf0ce1613
cansev <mee.cansev@gmail.com>
2024-01-16 16:18:11 +0300

    feat: adding spline effect implementation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Spline scene="https://prod.spline.design/a-E95Ntp8warDQI4/scene.splinecode" />
+      <a target="_blank" href="https://github.com/erencansev" rel="noreferrer">
+        <i class="fa-brands fa-github"></i> erencansev
+      </a>
+    </div>
+  );
+}
